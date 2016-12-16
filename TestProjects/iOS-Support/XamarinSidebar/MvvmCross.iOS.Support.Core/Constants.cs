570ab376dc15f13ebc6fa1ed6c094bf84144660f
Flavius Demian <flavius.demian@deventure.co>
2016-12-16 21:49:39 +0200

    updated to public env settings

--- TestProjects/iOS-Support/XamarinSidebar/MvvmCross.iOS.Support.Core/Constants.cs
+++ TestProjects/iOS-Support/XamarinSidebar/MvvmCross.iOS.Support.Core/Constants.cs
@@ -9,8 +9,8 @@ namespace MvvmCross.iOS.Support.XamarinSidebarSample.Core
     public class Constants
     {
         public const string EMPLOYER_ID = "9EDFCF04-06AC-44DF-A3A9-A6327D646A24";
-        //public const string WEB_API_BASE_ROOT_URL = "http://upworky.northeurope.cloudapp.azure.com:88";
-        public const string WEB_API_BASE_ROOT_URL = "http://192.168.1.112:81";
+        public const string WEB_API_BASE_ROOT_URL = "http://upworky.northeurope.cloudapp.azure.com:88";
+        //public const string WEB_API_BASE_ROOT_URL = "http://192.168.1.112:81";
 
         public const string VISIBILITY = "Visibility";
         public const string INVERTED_VISIBILITY = "InvertedVisibility";
